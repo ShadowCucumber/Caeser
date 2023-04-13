aa8557a42d159d20973b36a72bad648a8ee872b6
Mattis Henzel <111503864+ShadowCucumber@users.noreply.github.com>
2023-04-13 12:29:38 +0200

    Initial commit

---
+++ src/main/java/com/example/verschlusselung/VerschlusselungApplication.java
@@ -0,0 +1,13 @@
+package com.example.verschlusselung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerschlusselungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerschlusselungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/verschlusselung/VerschlusselungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verschlusselung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerschlusselungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
